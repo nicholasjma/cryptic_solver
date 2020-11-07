0ecf23b308e6ab1d7e51e431d4221dc6a8959687
Nicholas Ma <chitinid@gmail.com>
2020-11-07 16:55:33 -0600

    implement sandwich sudoku solver

--- cryptic_solver.py
+++ cryptic_solver.py
@@ -47,7 +47,7 @@ class SudokuCondition:
         pass
 
     def __call__(self, grid: np.ndarray, num: int, row: int, col: int) -> bool:
-        """Syntactic sugar to treat class isntance as function, see `test()`"""
+        """Syntactic sugar to treat class instance as function, see `test()`"""
         return self.test(grid, num, row, col)
 
 
@@ -138,6 +138,83 @@ class ConsecutiveCondition(SudokuCondition):
         )
 
 
+class ThermometerCondition(SudokuCondition):
+    def __init__(self, paths: list):
+        """
+        Parameters
+        ----------
+        paths: list of list of tuples
+            List of list of coordinates that should have increasing numbers
+        """
+        self.paths = paths
+
+    def test(self, grid: np.ndarray, num: int, row: int, col: int) -> bool:
+        for path in self.paths:
+            nums = [grid[row, col] for row, col in path if grid[row, col] > 0]
+            if not np.all(np.diff(nums) > 0):
+                return False
+        return True
+
+
+class SandwichCondition(SudokuCondition):
+    def __init__(self, row_sums: np.array, col_sums: np.array):
+        """
+        Parameters
+        ----------
+        row_sums: np.ndarray
+            array of sum of numbers sandwiched between 1 and 9 in each row
+        col_sums: np.ndarray
+            array of sum of numbers sandwiched between 1 and 9 in each column
+        """
+        self.row_sums = row_sums
+        self.col_sums = col_sums
+
+    def test(self, grid: np.ndarray, num: int, row: int, col: int) -> bool:
+        for grid_iter, rc_sum in zip([grid, grid.T], [self.row_sums, self.col_sums]):
+            for row, row_sum in enumerate(rc_sum):
+                continue
+                one_pos = np.where(grid_iter[row, :] == 1)[0]
+                nine_pos = np.where(grid_iter[row, :] == 9)[0]
+                if len(one_pos) == 0 or len(nine_pos) == 0:
+                    print("missing 1 or 9")
+                    continue
+                else:
+                    start = min(one_pos, nine_pos)[0] + 1
+                    stop = max(one_pos, nine_pos)[0]
+                    extent_checks = [
+                        (0, 1, 0),
+                        (1, 5, 1),
+                        (5, 9, 2),
+                        (9, 14, 3),
+                        (14, 20, 4),
+                        (20, 27, 5),
+                        (27, 35, 6),
+                    ]
+                    # check that 1 and 9 aren't too far apart
+                    for min_sum, max_sum, max_len in extent_checks:
+                        if (
+                            row_sum >= min_sum
+                            and row_sum < max_sum
+                            and start - stop > max_len
+                        ):
+                            return False
+                    nums = grid_iter[row, start:stop]
+                    if start == stop:  # 1 and 9 adjacent
+                        if row_sum > 0:
+                            return False
+                        else:
+                            continue
+                    elif nums.min() == 0:  # 1 and 9 present, holes between
+                        if nums.sum() > row_sum:  # sum is to high
+                            return False
+                        else:  # sum is okay thusfar
+                            continue
+                    else:  # 1 and 9 present, no holes between
+                        if nums.sum() != row_sum:
+                            return False
+        return True
+
+
 class ComboCondition(SudokuCondition):
     """Combine multiple SudokuCondition classses into one"""
 
@@ -147,7 +224,10 @@ class ComboCondition(SudokuCondition):
     def test(self, grid: np.ndarray, num: int, row: int, col: int) -> bool:
         try:
             for condition in self.conditions:
-                assert condition().test(grid, num, row, col)
+                if isinstance(condition, type):
+                    assert condition()(grid, num, row, col)
+                else:
+                    assert condition(grid, num, row, col)
         except AssertionError:
             return False
         else:
@@ -301,7 +381,7 @@ class SudokuSolver:
         """
         result = self._solve(grid)
         if result is None:
-            raise "No solution"
+            raise ValueError("No solution")
         else:
             return result
 
@@ -370,6 +450,73 @@ class CrypticSolver(SudokuSolver):
         )
 
 
+class SandwichSolver(SudokuSolver):
+    """
+    Sandwich sudoku solver. See
+    https://www.theguardian.com/science/2019/may/06/can-you-solve-it-sandwich-sudoku-a-new-puzzle-goes-viral
+    """
+
+    def __init__(self, grid: np.array, row_sums: np.array, col_sums: np.array):
+        """
+        Parameters
+        ----------
+        grid: np.ndarray
+            Grid representing puzzle, should be 9x9 array. Use 0 for unspecified entries
+        """
+        # initialize using standard ruleset
+        super().__init__(
+            grid,
+            ComboCondition(
+                [RookCondition, BlockCondition, SandwichCondition(row_sums, col_sums)]
+            ),
+        )
+
+    def possible_cols(self, grid: np.array, num: int, row: int) -> list:
+        return [
+            col
+            for col in range(9)
+            if grid[row, col] == 0 and self.condition(grid, num, row, col)
+        ]
+
+    def _solve(self, grid: np.ndarray = None) -> np.ndarray:
+        # Make sure we don't mutate the input
+        if grid is None:
+            grid = self.grid.copy()
+        else:
+            grid = grid.copy()
+        clear_output(wait=True)
+        print(grid)
+        nums, counts = np.unique(grid, return_counts=True)
+        if not hasattr(self, "num_order"):
+            num_order = sorted(range(len(nums)), key=counts.__getitem__, reverse=True)
+            num_order = [nums[x] for x in num_order]
+            num_order = num_order + [x for x in range(10) if x not in num_order]
+            num_order = [x for x in num_order if x != 0]
+            self.num_order = [x for x in num_order if x in {1, 9}] + [
+                x for x in num_order if x not in {1, 9}
+            ]
+        for test_num in self.num_order:
+            if np.count_nonzero(grid.copy() == test_num) < 9:
+                for row in range(10):
+                    if test_num in grid[row, :]:
+                        continue
+                    cols = self.possible_cols(grid, test_num, row)
+                    if len(cols) == 0:
+                        return
+                    else:
+                        for col in cols:
+                            grid_new = grid.copy()
+                            grid_new[row, col] = test_num
+                            result = self._solve(grid_new)
+                            if result is not None:
+                                return result
+                            else:
+                                continue
+                        return
+        if self.check_grid(grid):
+            return grid
+
+
 # In[2]:
 
 
@@ -390,3 +537,28 @@ cs = CrypticSolver(
 )
 
 cs.solve()
+
+
+# In[3]:
+
+
+cs = SandwichSolver(
+    np.array(
+        [
+            [6, 1, 2, 9, 3, 8, 4, 7, 5],
+            [7, 8, 3, 4, 5, 1, 2, 6, 9],
+            [0, 0, 9, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 2, 0, 1, 0],
+            [0, 0, 0, 1, 0, 0, 7, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        ]
+    ),
+    row_sums=np.array([2, 8, 26, 29, 0, 23, 15, 2, 4]),
+    col_sums=np.array([10, 23, 23, 23, 14, 12, 21, 0, 0]),
+)
+
+cs.solve()
+
